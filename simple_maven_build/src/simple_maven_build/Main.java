ad084f4ef12027aa98ae90eab8213f366915f8fc
HelenN <yami.kaze@gmail.com>
2017-06-20 12:10:34 -0700

    simple maven build

---
+++ simple_maven_build/src/simple_maven_build/Main.java
@@ -0,0 +1,14 @@
+package simple_maven_build;
+
+/**
+ * simple java project that is converted to maven project
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello world!");
+
+	}
+
+}
